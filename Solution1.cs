c324ef27f99b054d693177e317dea52160fb94e6
astillucf <44243476+astillucf@users.noreply.github.com>
2021-10-05 15:48:41 -0400

    Update Solution1.cs

--- Solution1.cs
+++ Solution1.cs
@@ -8,8 +8,13 @@ namespace ProjectEulerSolutionsAMS
         {
 
             //Problem 1
-            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.The sum of these multiples is 23.
-            //Find the sum of all the multiples of 3 or 5 below 1000.
+            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these 
+            //multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+            
+            //Solution Description
+            //This solution loops through each number between 1 and 999. If the number is either a multiple of 3 or 5, the 
+            //running total for sum of multiples is incremented by that number. The solution uses an else if statement to 
+            //prevent a number from being added more than once.
 
             int counter = 0;
             int sumOfMultiples = 0;
